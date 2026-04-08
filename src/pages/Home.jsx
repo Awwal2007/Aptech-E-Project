1d5495eb5d2bce71f51e7727dd1f31e3ee1d7d1e
village-coders <villagecoders7@gmail.com>
2026-04-08 21:45:12 +0100

    jdhdijc

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
       <AboutSection />
       <section className='find-us-on-map'>
         <div className='heading'>
-          <span>Showroom</span>
+          <span>SHOWROOM</span>
           <h1 className='GET-IN'>FIND US ON THE MAP</h1>
         </div>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3956.533563777467!2d3.9098500742379665!3d7.406041612280431!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1039930067851fff%3A0x8ea29ec1f2186eb8!2sWest%20One!5e0!3m2!1sen!2sng!4v1775646914276!5m2!1sen!2sng" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
